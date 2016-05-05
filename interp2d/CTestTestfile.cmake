3a7af8e845ce9cd7aec31c88d227b7891681b917
vskokov <vladi@skokov.net>
2016-05-05 12:58:31 -0400

    The long. momenta of patons and their energies are computed for output in the laboratory frame

--- interp2d/CTestTestfile.cmake
+++ interp2d/CTestTestfile.cmake
@@ -1,7 +1,7 @@
 # CMake generated Testfile for 
-# Source directory: /home/vskokov/Dropbox/Projects/2016/McDijet2016/MC_DiJet/interp2d
-# Build directory: /home/vskokov/Dropbox/Projects/2016/McDijet2016/MC_DiJet/interp2d
+# Source directory: /Users/vskokov/Dropbox/Projects/2016/McDijet2016/new/McDijet/interp2d
+# Build directory: /Users/vskokov/Dropbox/Projects/2016/McDijet2016/new/McDijet/interp2d
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(all "interp2dtest")
+add_test(all "interp2dtest")
